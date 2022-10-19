0bf5a15f0f12a51ca709c232e92cb98ca3c44dde
Jerzy Konieczny <62672370+jkoniecznyy@users.noreply.github.com>
2022-10-19 11:13:30 +0200

    Fix: Code refactor

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import random
-from typing import Set, List
 
-# Lista z obrazka
+# lista z obrazka
 example_list = [[0, 1, 0, 0, 1, 0],
                 [1, 1, 0, 0, 1, 0],
                 [0, 1, 0, 0, 0, 0],
@@ -10,28 +9,20 @@ example_list = [[0, 1, 0, 0, 1, 0],
                 [0, 0, 0, 0, 0, 0]]
 
 
-def createMap() -> List:
+def create_map() -> list:
     """
     Creates a random map of 0's and 1's (water/land)
     with a random number of rows and columns (both between 3 and 9)
 
     #TODO Na pewno można ulepszyć jeżeli chcemy
 
-    :return: List
+    :return: list
     """
-    rows = random.randint(3, 9)
-    columns = random.randint(3, 9)
-    map = []
-    row = []
-    for i in range(rows):
-        for y in range(columns):
-            row.append(random.randint(0, 1))
-        map.append(row)
-        row = []
-    return map
+    rows, columns = random.randint(3, 9), random.randint(3, 9)
+    return [[random.randint(0, 1) for y in range(columns)] for _ in range(rows)]
 
 
-def drawMap(map: List) -> None:
+def draw_map(map: list) -> None:
     """
     Draws the map in the console
     ' ' = Water
@@ -42,19 +33,10 @@ def drawMap(map: List) -> None:
     :param map:
     :return: None
     """
-    for row_nr in range(len(map)):
-        print('| ', end='')
-
-        for column_nr in range(len(map[row_nr])):
-            if map[row_nr][column_nr]:
-                print('M ', end='')
-            else:
-                print('  ', end='')
-
-        print('|')
+    [print('| ' + ' '.join(['M' if bit else ' ' for bit in row]) + ' |') for row in map]
 
 
-def countIslands(map: List) -> int:
+def count_islands(map: list) -> int:
     """
     Goes through the map detecting the land
     When the land is detected calls the function explore
@@ -67,20 +49,15 @@ def countIslands(map: List) -> int:
     """
     visited = set()
     count = 0
-    for row_nr in range(len(map)):
-        row = map[row_nr]
-        # print(f'row {row_nr}:  {row}')
-
+    for row_nr, row in enumerate(map):
         for column_nr in range(len(row)):
-            cell = row[column_nr]
-            # print(f'cell {row_nr},{column_nr} - value {cell}')
             if explore(map, row_nr, column_nr, visited):
                 count += 1
 
     return count
 
 
-def explore(map: List, row_nr: int, column_nr: int, visited: Set):
+def explore(map: list, row_nr: int, column_nr: int, visited: set):
     """
     Explores the island to mark all of its land
 
@@ -105,15 +82,24 @@ def explore(map: List, row_nr: int, column_nr: int, visited: Set):
         visited.add(position)
 
     explore(map, row_nr, column_nr - 1, visited)  # left
+    explore(map, row_nr + 1, column_nr - 1, visited)  # left down
     explore(map, row_nr + 1, column_nr, visited)  # down
+    explore(map, row_nr + 1, column_nr + 1, visited)  # down right
     explore(map, row_nr, column_nr + 1, visited)  # right
+    explore(map, row_nr - 1, column_nr + 1, visited)  # right up
     explore(map, row_nr - 1, column_nr, visited)  # up
+    explore(map, row_nr - 1, column_nr - 1, visited)  # up left
     return True
 
 
-# print(countIslands(example_list))
-m1 = createMap()
+# print(count_islands(example_list))
+m1 = create_map()
 print('Map preview:')
-drawMap(m1)
+draw_map(m1)
 print()
-print('Islands found: ', countIslands(m1))
+print('Islands found: ', count_islands(m1))
+
+print('Map preview:')
+draw_map(example_list)
+print()
+print('Islands found: ', count_islands(example_list))
